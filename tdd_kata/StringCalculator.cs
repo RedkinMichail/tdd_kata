9ab0465888a7cdd9e059b622de78d30e082cefe1
unknown <redkin.michail.a@gmail.com>
2017-11-02 19:32:01 +0300

    first test

---
+++ tdd_kata/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace tdd_kata
+{
+    internal static class StringCalculator
+    {
+        public static int Add(string input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ tdd_kata/WhenInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace tdd_kata
+{
+    [TestFixture]
+    class WhenInput
+    {
+        [Test]
+        public void EmptyString_ResultShouldBeZero()
+        {
+            var input = "";
+
+            var result = StringCalculator.Add(input);
+
+            Assert.AreEqual(0, result);
+        }
+}
+
+    internal static class StringCalculator
+    {
+        public static int Add(string input)
+        {
+            return 0;
+        }
+    }
+}
